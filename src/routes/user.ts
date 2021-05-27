2e669fc4948bbe5febd7e2e7741943dbad461033
bpiesliakaite <bpiesliakaite@gmail.com>
2021-05-27 23:48:06 +0300

    routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { authenticateRequest } from '../controllers/auth/authController';
-import { getSelf } from '../controllers/user/userController';
+import { changePassword, getSelf, updateUser } from '../controllers/user/userController';
 import { notAllowedHandler } from '../utils/route-handlers';
 
 const router = Router();
 
 router.get('/self', authenticateRequest, getSelf);
 router.all('/self', notAllowedHandler);
+
+router.post('/update', authenticateRequest, updateUser);
+router.all('/update', notAllowedHandler);
+router.post('/change-password', authenticateRequest, changePassword);
+router.all('/change-password', notAllowedHandler);
 
 export default router;
